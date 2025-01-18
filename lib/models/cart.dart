51aba468b5cfb02c5fdc3a80876bfb03a82fbd85
Dumeje <dum_obieli@yahoo.com>
2025-01-18 11:31:06 +0100

    Completed the shop page

---
+++ lib/models/cart.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:retale/models/shoe.dart';
+
+class Cart extends ChangeNotifier {
+  // list of shoes for sale
+  List<Shoe> shoeShop = [
+    Shoe(
+        name: 'Air Jordan',
+        price: '245',
+        imagePath: 'lib/images/Nike Air Jordan.png',
+        description:
+            "You've got the hops and speed-lace up in shoes that enhance your performance."),
+    Shoe(
+      name: 'Louis Vuitton',
+      price: '250',
+      imagePath: 'lib/images/Nike & LV.png',
+      description: 'The forward-thinking design of his latest signature shoe.',
+    ),
+    Shoe(
+      name: 'Air Strip',
+      price: '240',
+      imagePath: 'lib/images/Nike Air Strip.png',
+      description:
+          'A secure midfootstrap suited for scoring binges and defensive plays',
+    ),
+    Shoe(
+      name: 'KD Treys',
+      price: '255',
+      imagePath: 'lib/images/Nike shoe.png',
+      description: 'Bouncy cushioning paired with soft and supportive foam.',
+    ),
+  ];
+
+  // list of items in user cart
+  List<Shoe> userCart = [];
+
+  // get list of shoes for sale
+  List<Shoe> getShoeList() {
+    return shoeShop;
+  }
+
+  // get cart
+  List<Shoe> getUserCart() {
+    return userCart;
+  }
+
+  // add items to cart
+  void addItemToCart(Shoe shoe) {
+    userCart.add(shoe);
+    notifyListeners();
+  }
+
+  // remove item from cart
+  void removeItemFromCart(Shoe shoe) {
+    userCart.remove(shoe);
+    notifyListeners();
+  }
+}
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:retale/components/shoe_tile.dart';
+import 'package:retale/models/cart.dart';
+import 'package:retale/models/shoe.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -10,8 +14,96 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('shop'),
+    return Consumer<Cart>(
+      builder: (context, value, child) => Column(
+        children: [
+          // search bar
+          Container(
+            padding: const EdgeInsets.all(12),
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Search',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                Icon(
+                  Icons.search,
+                  color: Colors.grey,
+                ),
+              ],
+            ),
+          ),
+
+          // message
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25.0),
+            child: Text(
+              'everyone flies... some fly longer than others.',
+              style: TextStyle(
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
+          // hot picks
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  'Hot Picks 🔥',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  'See all',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 10),
+
+          // list of shoes for sale
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                // get a shoe from shop list
+                Shoe shoe = value.getShoeList()[index];
+
+                // return the shoe
+                return ShoeTile(
+                  shoe: shoe,
+                );
+              },
+            ),
+          ),
+
+          const Padding(
+            padding: EdgeInsets.only(top: 25, left: 25, right: 25),
+            child: Divider(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
